2c068b6bf4f066c876f6bbda2f846cfe24a56ac2
Smile259 <smile02006@outlook.com>
2025-09-07 12:35:52 +0530

    Added calculator

---
+++ Calculator.py
@@ -0,0 +1,15 @@
+while True:
+    a = float(input("Enter first number:"))
+    op = input("Enter operator(+,-,*,/):")
+    b = float(input("Enter second number:"))
+
+    if op == "+":
+        print("Result =", a + b)
+    elif op == "-":
+        print("Result =", a - b) 
+    elif op == "*":
+        print("Result =", a *  b)
+    elif op == "/":
+        print("Result =", a / b if b != 0 else "Error! Division by zero")
+    else:
+        print("Invalid operator!")
